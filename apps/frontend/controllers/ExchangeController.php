18feab2731c2a8d09048a758c455e16eb9bba6ee
thachphong <whirlwind2887@gmail.com>
2017-10-13 00:25:47 +0700

    fix dien tich

--- apps/frontend/controllers/ExchangeController.php
+++ apps/frontend/controllers/ExchangeController.php
@@ -4,6 +4,7 @@ namespace Multiple\Frontend\Controllers;
 
 use Multiple\PHOClass\PHOController;
 use Multiple\Models\Exchange;
+use Multiple\Models\ExchangeReceive;
 use Multiple\Models\Define;
 use Multiple\PHOClass\PhoLog;
 use Multiple\Library\FilePHP;
@@ -28,28 +29,16 @@ class ExchangeController extends PHOController
 		$param['amount'] = str_replace(',','',$param['amount']);
 		//PhoLog::debug_var('---param---',$param);
 		$param['currency']= 'VND';
+		//$param['payment_type'] = 'VTCPay';
 		//$param['reference_number']= 'VND';
 		//PhoLog::debug_var('---param---',$param);
-		$cache = $this->createCache(['lifetime' => 86400 ]); //1 ngay
-        $cachekey ="paysend.cache";
-        $cdata = $cache->get($cachekey);
-        //PhoLog::debug_var('---param---',$param);
-        //PhoLog::debug_var('---param---',__LINE__);
-        if($cdata == null){
-        	$df = new Define();
-        	$res = $df->get_list_bygroup_all(1);
-        	foreach($res as $row){
-				$cdata[$row['define_key']] = $row['define_val'];			
-			}
-        	//PhoLog::debug_var('---param---',__LINE__);
-			$cache->save($cachekey,$cdata);
-			//PhoLog::debug_var('---param---',$cdata);
-		}
+		$cdata = $this->get_define();
 		//PhoLog::debug_var('---param---',__LINE__);
 		$user = $this->session->get('auth');
 		//PhoLog::debug_var('---param---',__LINE__);
 		//PhoLog::debug_var('---param---',$param);
-		$param['receiver_account']= $cdata['pay_vtc_account'];		
+		$param['receiver_account']= $cdata['pay_vtc_account'];	
+		$param['reference_number']='';	
 		$param['website_id']= $cdata['pay_website_id'];		
 		$url_checkout = $cdata['pay_url_checkout'].'?';
 		$par_ins = $param;
@@ -59,9 +48,9 @@ class ExchangeController extends PHOController
 		$db = new Exchange();
 		
 		$param['reference_number'] = $db->_insert($par_ins);
-		$arr_key = array_keys($param);
+		//$arr_key = array_keys($param);
 		
-		$param['signature']= implode('|',$arr_key).'|'.$cdata['pay_securitycode'];
+		$param['signature']= strtoupper(hash('sha256',  implode('|',$param).'|'.$cdata['pay_securitycode']));
 		
 		$data =array();
 		foreach($param as $key =>$val){
@@ -73,116 +62,43 @@ class ExchangeController extends PHOController
 		return $this->response->redirect($url_checkout);
 		
 	}
-	public function loginAction(){
-		$this->set_template_share();
-	}
-	public function activeAction(){
-		$param = $this->get_Gparam(array('email','rd'));
-		$db = new Users();
-		if($db->active($param['email'],$param['rd'])){
-			$res['active_msg'] = 'Kích hoạt tài khoản thành công !';
-		}else{
-			$res['active_msg'] = 'Link kích hoạt tài khoản không đúng !';
-		}		
-		$this->set_template_share();
-		$this->ViewVAR($res);
-	}
-	public function logoutAction()
+	public function recieveAction()
 	{
-		$this->session->set('auth', null);
-        return $this->_redirect('dang-nhap');
-	}
-	
-	
-	public function updateAction(){
-		$param = $this->get_param(array('user_no','user_name','email','mobile','address','pass','sex'));
-		$result = array('status' => 'OK');
-		$result['status'] = 'OK';	
-		$result['msg'] = 'Cập nhật thành công!';		
-		$db = new Users();
-	
-		$msg = $db->get_validation($param);
-		
-		if($msg === true){
-			$db->_insert($param);
-			$this->sendmail($db);
-		}else{			
-			$result = $msg;
-			$result['status'] = 'NOT';	
-		}
-		return $this->ViewJSON($result);
-	}
-	public function sendmail($usr){
-		$email = new Mail();
-		
-		$body_tmp = $this->get_body($usr);		
-		$replacements['HEADER'] = '<h3><strong>Chúc mừng bạn đã đăng kí thành công tài khoản trên Datxanhviet.vn!</strong></h3>';
-		$replacements['BODY'] = $body_tmp;
-		$db = new Define();
-		$data = $db->get_info(DEFINE_KEY_EMAIL);
-		//$mail_to[]['mail_address']= $data->define_val;
-		$mail_to[]['mail_address']= $usr->email;	
-		//ACWLog::debug_var('--mail',$mail_to);	
-		//ACWLog::debug_var('--mail',$data);
-		$email->AddListAddress($mail_to);
-		$email->add_replyto($data->define_val,'datxanhviet.vn');
-                
-		$email->Subject = 'Đăng ký tài khoản thành công - '.date('d/m/Y H:i:s');                
-		$email->loadbody('template_mail.html');
-		$email->replaceBody($replacements);
-		$result = $email->send();
-	}
-	public function get_body($usr){
-		$url = BASE_URL_NAME."users/active?email=".$usr->email."&rd=".$usr->user_id;
-		$html="<p>Dưới đây là thông tin đăng nhập của bạn: </p>
-			   <p><strong>Tên đăng nhập:</strong> ".$usr->user_no."</p>
-			   <p><strong>Email:</strong> ".$usr->email."</p>
-			   <p><strong>Điện thoại:</strong> ".$usr->mobile." </p>
-			   <br/>
-			   <p>Vui lòng kích vào đường link dưới đây để kích hoạt tài khoản của bạn:</p>
-			  <a href='".$url ."'>".$url ."</a> 
-
-			<p>Nếu đường link trên không hoạt động, vui lòng copy đường link đó, rồi paste lên thanh địa chỉ của trình duyệt để link tới trang kích hoạt trên hệ thống. </p>
-			<p>Cảm ơn bạn đã sử dụng dịch vụ của chúng tôi!</p>";
-		return $html;
-	}
-	public function updatepassAction(){
-		$param = $this->get_param(array('pass_old','pass_new'));
-		$result = array('status' => 'OK');
-		$result['status'] = 'OK';	
-		$result['msg'] = 'Cập nhật thành công!';	
-		$user = $this->session->get('auth');	
-		$db = new Users();
-		if($db->get_user($user->user_no,$param['pass_old']) != FALSE){
-			$db->updatepass($user->user_id,$param['pass_old'],$param['pass_new']);			
+		$param = $this->get_param('data','signature');
+			
+		$data = explode('|',$param['data']);
+		$param['amount']= $data[0];
+		$param['message']= $data[1];
+		$param['payment_type'] = $data[2];
+		$param['reference_number'] = $data[3];
+		$param['status'] = $data[4];
+		$param['trans_ref_no'] = $data[5];
+		$param['website_id'] = $data[6];
+		$cdata = $this->get_define();
+		$plaintext = $param['data'].'|'.$cdata['pay_securitycode'];
+		$mysign = strtoupper(hash('sha256', $plaintext));
+		PhoLog::debug_var('---recieve---',$param);
+		if($mysign != $param['signature']){
+			echo "kết quả ko hợp lệ !";
 		}else{
-			$result['status'] = 'NOT';	
-			$result['msg'] = 'Mật khẩu cũ không đúng!';
+			$db = new ExchangeReceive();
+			$db->_insert($param);
 		}
-				
-		return $this->ViewJSON($result);
+		
 	}
-	public function updateinfoAction(){
-		$param = $this->get_param(array('user_name','address','mobile','facebook','skype','sex','avata','folder_tmp'));
-		$result = array('status' => 'OK');
-		$result['status'] = 'OK';	
-		$result['msg'] = 'Cập nhật thành công!';	
-		$user = $this->session->get('auth');	
-		$db = new Users();
-		$param['user_id'] = $user->user_id;
-		if(strlen($param['avata'])>0){
-			$file = new FilePHP();
-			$ext = $file->GetExtensionName($param['avata']);
-			if($user->avata != ''){
-				$file->DeleteFile(PHO_PUBLIC_PATH.'images/users/'.$user->avata);
-			}	
-			$file_name = uniqid('',TRUE).'.'.$ext;	
-			$file->CopyFile(PHO_PUBLIC_PATH.$param['avata'],PHO_PUBLIC_PATH.'images/users/'.$file_name);
-			$file->DeleteFolder(PHO_PUBLIC_PATH.$param['folder_tmp']);
-			$param['avata'] =$file_name;
+	public function get_define(){
+		$cache = $this->createCache(['lifetime' => 86400 ]); //1 ngay
+        $cachekey ="paysend.cache";
+        $cdata = $cache->get($cachekey);        
+        if($cdata == null){
+        	$df = new Define();
+        	$res = $df->get_list_bygroup_all(1);
+        	foreach($res as $row){
+				$cdata[$row['define_key']] = $row['define_val'];			
+			}        	
+			$cache->save($cachekey,$cdata);			
 		}
-		$db->updateinfo($param);
-		$this->_registerSession($db->get_info($user->user_id));
-		return $this->ViewJSON($result);
+		return $cdata;
 	}
+	
 }
--- apps/models/ExchangeRecieve.php
+++ apps/models/ExchangeRecieve.php
@@ -5,73 +5,34 @@ namespace Multiple\Models;
 use Multiple\Models\DBModel;
 use Multiple\PHOClass\PHOArray;
 
-class Slide extends DBModel
+class ExchangeReceive extends DBModel
 {
-    public $slide_id;
-    public $slide_type;
-    public $img_path;
-    public $del_flg;
-    public $add_date;
-    public $add_user;
-    public $upd_date;
-    public $upd_user;
-    public $link_page;
-    public $banner_flg;
+    public $exchange_receive_id;
+    public $data;
+    public $signature;
+    public $amount;
+    public $message;
+    public $payment_type;
+    public $reference_number;
+    public $status;
+    public $trans_ref_no;
+    public $website_id;
     public function initialize()
     {
-        $this->setSource("slides");
+        $this->setSource("exchange_receive");
     }        
-   
-	public function get_all(){
-         $usr_data = Slide::find(array('del_flg'=>0));
-    }
-    public function get_row($slide_id){
-         return  Slide::findFirst(array('slide_id'=>$slide_id));
-    }
-    public function get_info($slide_id){
-         return  Slide::findFirst(array("slide_id = :slide_id:",
-            'bind' => array('slide_id' => $slide_id)
-        ));
-    }
-    public function get_slides_all($banner_flg= 0)
-	{		
-		return Slide::find(array("banner_flg = :banner_flg:",
-            'bind' => array('banner_flg' => $banner_flg)
-        ));
+    public function _insert($param){
+		$this->data=$param['data'];
+		$this->signature=$param['signature'];
+		$this->amount=$param['amount'];
+		$this->message=$param['message'];
+		$this->payment_type=$param['payment_type'];
+		$this->reference_number=$param['reference_number'];
+		$this->website_id=$param['website_id'];
+		$this->status=$param['status'];
+		$this->trans_ref_no=$param['trans_ref_no'];
+		$this->save();
 	}
-	public function get_slides_list($banner_flg= 0)
-	{		
-		return Slide::find(array("banner_flg = :banner_flg: and del_flg =0",
-            'bind' => array('banner_flg' => $banner_flg)
-        ));
-	}
-	public function _insert($param){
-		//$this->slide_id = $param['slide_id'];
-	    $this->slide_type= 0;
-	    $this->img_path= $param['img_path'];
-	    $this->del_flg= 0;	   
-	    $this->add_user= $param['user_id'];	  
-	    $this->upd_user= $param['user_id'];
-	    $this->link_page= $param['link_page'];
-	    $this->banner_flg= $param['banner_flg'];
-	    $this->save();
-	}
-	public function _update($param){
-		$sql = "update slides
-					set img_path = :img_path
-					,del_flg = :del_flg	
-					,link_page= :link_page				
-					,upd_date =  now()
-					,upd_user =:user_id					
-					where slide_id = :slide_id
-				";	
- 
-		$this->pho_execute($sql, PHOArray::filter($param, array(
-					'slide_id'
-					,'img_path'					
-					,'user_id'
-					,'del_flg'	
-					,'link_page'			
-					)));	
-	}
+	
+	
 }
